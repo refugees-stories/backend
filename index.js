9dba60a553d1f60c28e7a74469c4149e929e605e
john2796 <jbmiranda22796@gmail.com>
2019-06-25 14:23:23 -0700

    added .env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const server = require("./api/server.js");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`/*** Server Running on ${port} ***/`);
